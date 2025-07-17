f23a8ffd49a1373fc199915db6ee1fdd5b823773
William3110-coder <68011516@kmitl.ac.th>
2025-07-17 11:30:24 +0000

    challenge exercise lab-2(recode)

--- lab-2/challenge.c
+++ lab-2/challenge.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 int main()
 {
     float num1, num2, num3;
@@ -10,8 +11,14 @@ int main()
     printf("Enter integer: ");
     scanf("%f", &num3);
     printf("Results: ");
-    printf("\nMinimum: %.0f", num2);
-    printf("\nMaximum3: %.0f", num3);
+    float min = num1;
+    if(num2 < min) min = num2;
+    if(num3 < min) min = num3;
+    float max = num1;
+    if(num2 > max) max = num2;
+    if(num3 > max) max = num3;
+    printf("\nMinimum: %.0f", min);
+    printf("\nMaximum3: %.0f", max);
     printf("\nSum: %.0f",num1+num2+num3);
     float x = (num1+num2+num3)/3;
     printf("\nAverage: %.2f\n",x);
